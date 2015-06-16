5df38db1dc7b3239a6d00e0516f2942077f97099
Harrison Hubbell <hhubbell@uvm.edu>
2015-06-15 21:36:43 -0400

    Fixed distribution headaches for now

--- setup.py
+++ setup.py
@@ -6,8 +6,21 @@ setup(
     description='A Python wrapper for the Magic Seaweed Marine Forecast API.',
     url='https://github.com/hhubbell/python-msw',
     author='Harry Hubbell',
-    install_requires=[
-        'requests'
+    install_requires=['requests'],
+    packages=[
+        'msw',
+        'msw.spots',
+        'msw.spots.africa',
+        'msw.spots.asia',
+        'msw.spots.australasia',
+        'msw.spots.caribbean',
+        'msw.spots.central_america',
+        'msw.spots.europe',
+        'msw.spots.indian_ocean',
+        'msw.spots.indonesia',
+        'msw.spots.middle_east',
+        'msw.spots.north_america',
+        'msw.spots.pacific_ocean',
+        'msw.spots.south_america'
     ]
-    packages=['msw']
 )
